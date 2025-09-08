b09fa8604b26855b60bc57d10364f38ae2158216
sirfara <aratihsirf@gmail.com>
2025-09-08 15:09:36 +0700

    week2

---
+++ week2/src/praktikum1.dart
@@ -0,0 +1,30 @@
+void main() {
+  String test = "test2";
+
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+
+  if (test == "test2") {
+    print("Test2 again");
+  }
+
+String status = "true";
+
+  if (status == "true") {
+    print("Kebenaran");
+  } else {
+    print("Bukan kebenaran");
+  }
+
+  String cek = "true";
+  if (cek == "true") {
+    print("Kebenaran");
+  } else {
+    print("Bukan kebenaran");
+  }
+}
